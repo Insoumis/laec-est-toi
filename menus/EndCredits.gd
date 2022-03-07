f4ea4c6ba16be5aace42b9c1a1b86ef11f3e0333
domi41 <dominique.merle.41@gmail.com>
2022-03-07 01:36:10 +0100

    feat: add some roles for the end credits
    Please, add your ideas!

--- menus/EndCredits.gd
+++ menus/EndCredits.gd
@@ -8,11 +8,104 @@ extends Control
 # 
 # Famille
 # 
-# 
-# 
+
+enum ROLES {
+	CODER
+	TOOLS
+#	GAME_MASTER
+	GAME_DESIGNER
+	LEVEL_DESIGNER
+	MUSIC_COMPOSER
+	MUSIC_PERFORMER
+	GRAPHIC_DESIGNER
+	TECHNICAL_ARTIST
+	SOUND_DESIGNER
+	ORGANIZER
+	REVIEWER
+	TESTER
+	THANKS
+	SPECIAL_THANKS
+}
+
+var people := [
+	
+	{
+		"name": "Fanelia",
+		"roles": ['thanks'],
+	},
+	{
+		"name": "Trollune",
+		"roles": ['code', 'levels'],
+	},
+	{
+		"name": "Precheurius",
+		"roles": ['music', 'orga'],
+	},
+	{
+		"name": "Adrenesis",
+		"roles": ['code'],
+	},
+	{
+		"name": "Miidnight",
+		"roles": ['orga', 'levels'],
+	},
+	{
+		"name": "Dazo",
+		"roles": ['levels'],
+	},
+	{
+		"name": "Bogoss69",
+		"roles": ['code', 'orga', 'music', 'thanks'],
+	},
+	{
+		"name": "Roipoussiere",
+		"roles": ['code'],
+	},
+	{
+		"name": "Karibou",
+		"roles": ['thanks'],
+	},
+	{
+		"name": "Zargett",
+		"roles": ['levels'],
+	},
+	{
+		"name": "SpyBob",
+		"roles": ['testing'],
+	},
+	{
+		"name": "Stheal",
+		"roles": ['levels'],
+	},
+	{
+		"name": "Niwatori",
+		"roles": ['pixels'],
+	},
+	{
+		"name": "Lametyste",
+		"roles": ['pixels'],
+	},
+	{
+		"name": "IvanC",
+		"roles": ['levels'],
+	},
+	{
+		"name": "AudreyH",
+		"roles": ['levels'],
+	},
+	{
+		"name": "Gobz",
+		"roles": ['thanks'],
+	},
+	{
+		"name": "JeanSebastienBachOfficiel",
+		"roles": ['testing'],
+	},
+
+]
 
 
-var pages = [
+var pages := [
 	{
 		'title': "",
 		'text': "",
