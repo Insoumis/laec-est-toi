c2ce1decd9bad7071b15871b336f81cc31a1d24a
Adrenesis <adrenesis@gmail.com>
2022-03-15 18:44:12 +0100

    feat: Make the credits' scene display data

--- menus/EndCredits.gd
+++ menus/EndCredits.gd
@@ -43,7 +43,7 @@ var people := [
 	},
 	{
 		"name": "Adrenesis",
-		"roles": [ROLES.CODER],
+		"roles": [ROLES.CODER, ROLES.TOOLS, ROLES.TECHNICAL_ARTIST],
 	},
 	{
 		"name": "Miidnight",
@@ -111,4 +111,52 @@ var pages := [
 		'text': "",
 	},
 ]
+
+var credits_holder
+
+func _process(delta):
+	credits_holder.margin_top -= delta * 50.0
+
+func _ready():
+	credits_holder = build_credit_controls(get_people_per_role())
+	add_child(credits_holder)
+	$ProRoleLabel.visible = false
+	$ProPersonLabel.visible = false
+	credits_holder.margin_top = 800.0
+
+func get_people_per_role():
+	var people_per_role = Dictionary()
+	for role in ROLES.values():
+		people_per_role[role] = Array()
+	for person in people:
+		for role in person.roles:
+			people_per_role[role].append(person)
+	return people_per_role
+
+func build_credit_controls(people_per_role : Dictionary):
+	var credits_holder = Control.new()
+	credits_holder.anchor_top = 0.0
+	credits_holder.anchor_left = 0.0
+	credits_holder.anchor_right = 1.0
+	credits_holder.anchor_bottom = 1.0
+	var credits_vbox = VBoxContainer.new()
+	credits_vbox.anchor_top = 0.0
+	credits_vbox.anchor_left = 0.0
+	credits_vbox.anchor_right = 1.0
+	credits_vbox.anchor_bottom = 1.0
+	credits_holder.add_child(credits_vbox)
+	for role in people_per_role.keys():
+		var role_label = Label.new()
+		role_label.align = Label.ALIGN_CENTER
+		role_label.text = "\n" + ROLES.keys()[role] + "\n"
+		role_label.set("custom_fonts/font", $ProRoleLabel.get("custom_fonts/font"))
+		credits_vbox.add_child(role_label)
+		for person in people_per_role[role]:
+			var person_label = Label.new()
+			person_label.align = Label.ALIGN_CENTER
+			person_label.text = person.name
+			person_label.set("custom_fonts/font", $ProPersonLabel.get("custom_fonts/font"))
+			credits_vbox.add_child(person_label)
+	return credits_holder
+	
 
