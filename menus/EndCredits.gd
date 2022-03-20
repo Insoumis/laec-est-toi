8394546bf6f361f174b56ffe4bdf9aecdb5f9969
domi41 <dominique.merle.41@gmail.com>
2022-03-20 13:19:36 +0100

    fix: end credits

--- menus/EndCredits.gd
+++ menus/EndCredits.gd
@@ -1,12 +1,6 @@
 extends Control
 
 
-# Développement
-# Pixels
-# Musique
-# Niveaux
-# 
-# Famille
 
 enum ROLES {
 	CODER
@@ -26,9 +20,11 @@ enum ROLES {
 	SPECIAL_THANKS
 }
 
+
+# By order of apparition, unless random is ON
 var role_names = {
 	ROLES.CODER : tr("Coder"),
-	ROLES.TOOLS : tr("Tool developper"),
+	ROLES.TOOLS : tr("Tool developer"),
 	ROLES.GAME_DESIGNER : tr("Game designer"),
 	ROLES.LEVEL_DESIGNER : tr("Level designer"),
 	ROLES.MUSIC_COMPOSER : tr("Music composer"),
@@ -51,7 +47,7 @@ var people := [
 	},
 	{
 		"name": "Trollune",
-		"roles": [ROLES.CODER],
+		"roles": [ROLES.CODER, ROLES.TOOLS, ROLES.TECHNICAL_ARTIST, ROLES.LEVEL_DESIGNER, ROLES.GRAPHIC_DESIGNER, ROLES.GAME_DESIGNER],
 	},
 	{
 		"name": "Precheurius",
@@ -75,7 +71,7 @@ var people := [
 	},
 	{
 		"name": "Roipoussiere",
-		"roles": [ROLES.CODER],
+		"roles": [ROLES.CODER, ROLES.GAME_DESIGNER],
 	},
 	{
 		"name": "Karibou",
@@ -91,7 +87,7 @@ var people := [
 	},
 	{
 		"name": "Stheal",
-		"roles": [ROLES.LEVEL_DESIGNER],
+		"roles": [ROLES.LEVEL_DESIGNER, ROLES.GAME_DESIGNER],
 	},
 	{
 		"name": "Niwatori",
@@ -117,65 +113,90 @@ var people := [
 		"name": "JeanSebastienBachOfficiel",
 		"roles": [ROLES.TESTER],
 	},
-
-]
-
-
-var pages := [
 	{
-		'title': "",
-		'text': "",
-	},
+		"name": "Saccharose4965",
+		"roles": [ROLES.TESTER],
+	}
 ]
 
-var credits_holder
+
+export var scroll_speed := 50.0
+
+
+# The scene (Start Menu) these End Credits redirect to once they're done
+export(String, FILE, "*.tscn") var forward_scene_path: String
+
+
+# Container (generated) for our generated nodes from the data above
+var credits_holder: Control
+var scroll_limit := 1000
+# Computed and set during ready, from contents' size
+
 
 func _process(delta):
-	credits_holder.margin_top -= delta * 50.0
+	self.credits_holder.margin_top -= delta * self.scroll_speed
+	if self.credits_holder.margin_top < -1 * self.scroll_limit:
+		set_process(false)
+		if self.forward_scene_path:
+			Game.go_back_to_first()
+			# todo
+			#Game.go_back_to_scene_path(self.forward_scene_path)
+
 
 func _ready():
-	credits_holder = build_credit_controls(get_people_per_role())
-	add_child(credits_holder)
+	self.credits_holder = build_credit_controls(get_people_per_role())
+	add_child(self.credits_holder)
+	# Disable the nodes used as templates during building above
 	$ProRoleLabel.visible = false
 	$ProPersonLabel.visible = false
-	credits_holder.margin_top = 800.0
+	# Position the credits below the screen (they'll scroll up)
+	self.credits_holder.margin_top = get_viewport().size.y
+	self.scroll_limit = self.credits_holder.find_node("CreditsVBox", true, false).rect_size.y
+
 
 func get_people_per_role():
 	var people_per_role = Dictionary()
 	for role in ROLES.values():
 		people_per_role[role] = Array()
-	for person in people:
+	for person in self.people:
 		for role in person.roles:
 			people_per_role[role].append(person)
 	return people_per_role
 
+
 func build_credit_controls(people_per_role : Dictionary):
-	var credits_holder = Control.new()
-	credits_holder.anchor_top = 0.0
-	credits_holder.anchor_left = 0.0
-	credits_holder.anchor_right = 1.0
-	credits_holder.anchor_bottom = 1.0
+	var wrapper = Control.new()
+	wrapper.name = "CreditsHolder"
+	wrapper.anchor_top = 0.0
+	wrapper.anchor_left = 0.0
+	wrapper.anchor_right = 1.0
+	wrapper.anchor_bottom = 1.0
 	var credits_vbox = VBoxContainer.new()
+	credits_vbox.name = "CreditsVBox"
 	credits_vbox.anchor_top = 0.0
 	credits_vbox.anchor_left = 0.0
 	credits_vbox.anchor_right = 1.0
 	credits_vbox.anchor_bottom = 1.0
-	credits_holder.add_child(credits_vbox)
-	for role in people_per_role.keys():
+	wrapper.add_child(credits_vbox)
+	var roles = people_per_role.keys()
+	randomize()
+	roles.shuffle()
+	for role in roles:
 		if people_per_role[role].size() > 0:
 			var role_label = $ProRoleLabel.duplicate()
 			role_label.align = Label.ALIGN_CENTER
 			var role_name = role_names[role].to_upper()
-			print(role_name)
 			role_label.text = "\n" + role_name + "\n"
 			role_label.set("custom_fonts/font", $ProRoleLabel.get("custom_fonts/font"))
 			credits_vbox.add_child(role_label)
-			for person in people_per_role[role]:
+			var role_people = people_per_role[role]
+			role_people.shuffle()
+			for person in role_people:
 				var person_label = $ProPersonLabel.duplicate()
 				person_label.align = Label.ALIGN_CENTER
 				person_label.text = person.name
 				person_label.set("custom_fonts/font", $ProPersonLabel.get("custom_fonts/font"))
 				credits_vbox.add_child(person_label)
-	return credits_holder
+	return wrapper
 	
 
