8a122324a68c1ee939db184ac93d2b8c85f08582
domi41 <dominique.merle.41@gmail.com>
2022-03-21 03:03:31 +0100

    docs

--- tools/LevelsIndexer.gd
+++ tools/LevelsIndexer.gd
@@ -1,5 +1,7 @@
 extends Control
 
+# old and dusty and not used but perhaps someday
+
 # This needs to be run whenever we add new levels that will matter for score
 # It takes too long to run on clients. (it instantiates ALL levels)
 
